90772ac8bf3639e1b9212500c96314cb619681c2
Mina Sami <minasami@diana-macbook-pro.local>
2021-12-08 09:48:19 +0100

    fix brew installation

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Make sure we’re using the latest Homebrew.
 brew update
